cdd328e8e465ed3eebe338273ee559bd4119ee60
邱浩楠 <Arthur>
2020-02-13 22:54:24 +0800

    .

--- draw_cifar.py
+++ draw_cifar.py
@@ -139,7 +139,7 @@ for data, target in test_loader:
     data, target = data.cuda(), target.cuda()
 
     adv_data = adversary_test(net, data, target)
-    # adv_data.requires_grad_()
+    adv_data.requires_grad_()
 
     # forward
     output = net(adv_data)
